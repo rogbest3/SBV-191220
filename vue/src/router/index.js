b31331ae3c885d6082513ea044bcf508384429f7
rogbest3 <rogbest3@gmail.com>
2019-12-26 09:36:23 +0900

    191226 09:35 moin CRUD, Layout ok

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,35 +1,7 @@
-/* import Vue from 'vue'
-import Router from 'vue-router'
-
-import Login from '@/components/member/Login.vue'
-import Join from '@/components/member/Join.vue'
-
-import List from '@/components/board/List.vue'
-import Remove from '@/components/board/Remove.vue'
-import Search from '@/components/board/Search.vue'
-import Update from '@/components/board/Update.vue'
-import Write from '@/components/board/Write.vue'
-
-Vue.use(Router)
-
-export default new Router({
-	mode : 'history',
-	routes : [
-
-		{ path : '/login', name : 'login', component : Login },
-		{ path : '/join', name : 'join', component : Join },
-		{ path : '/list', name : 'list', component : List },
-		{ path : '/remove', name : 'remove', component : Remove },
-		{ path : '/search', name : 'search', component : Search },
-		{ path : '/update', name : 'update', component : Update },
-		{ path : '/write', name : 'write', component : Write }
-	]
-}) */
-
 import Vue from 'vue'
 import Router from 'vue-router'
 // import Home from '@/components/Home'
-import Main1 from "@/components/main/Main1"
+import Main1 from "@/components/Main1"
 import Main2 from "@/components/main/Main2"
 import Login from "@/components/auth/Login"
 import Join1 from "@/components/auth/Join1"
@@ -40,12 +12,17 @@ import CusInfo from "@/components/mypage/CusInfo"
 import PwdChg from "@/components/mypage/PwdChg"
 import RefMgmt from "@/components/mypage/RefMgmt"
 import CusInfoChg from "@/components/mypage/CusInfoChg"
+// import Mypage from "@/components/Mypage"
+import Nav from "@/components/cmm/Nav"
+import PostAuth from "@/components/cmm/PostAuth"
 
 Vue.use(Router)
 
 export default new Router({
 	mode : 'history',
-	routes : [ { path : '/', name : 'mai1n', component : Main1 },
+	routes : [ { path : '/', name : 'mai1', component : Main1 },
+				{ path : '/nav', name : 'nav', component : Nav },
+				// { path : '/mypage', name : 'mypage', component : Mypage },
 				{ path : '/main2', name : 'main2', component : Main2 },
 				{ path : '/login', name : 'login', component : Login },
 				{ path : '/join1', name : 'join1', component : Join1 },
@@ -55,5 +32,8 @@ export default new Router({
 				{ path : '/cusinfo', name : 'cusinfo', component : CusInfo },
 				{ path : '/cusinfochg', name : 'cusinfochg', component : CusInfoChg },
 				{ path : '/pwdchg', name : 'pwdchg', component : PwdChg },
-				{ path : '/refmgmt', name : 'refmgmt', component : RefMgmt } ]
+				{ path : '/refmgmt', name : 'refmgmt', component : RefMgmt },
+				{ path : '/postAuth', name : 'postAuth', component : PostAuth }
+				//	children : [ { path : '/main2', name : 'main2', component : Main2 } ] }
+			]
 })
